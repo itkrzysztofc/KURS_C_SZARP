3c7b7f3f6e93b6c316e8831ef2b82544162e87e0
Krzysztof C <itkrzysztofc@hotmail.com>
2022-01-15 06:49:54 +0100

    v.1

---
+++ A07_Skok_Break_continue/Program.cs
@@ -0,0 +1,14 @@
+﻿// A06 Proste przykłady zastosowania wcześniejszych lekcji 
+
+using System;
+
+namespace A07_Skok_Break_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }  
+    }
+}

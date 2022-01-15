301414ae876a123960b9b59462021654e261d993
Krzysztof C <itkrzysztofc@hotmail.com>
2022-01-15 09:23:02 +0100

    Lekcja 8.

--- A08_Tablice/Program.cs
+++ A08_Tablice/Program.cs
@@ -8,7 +8,19 @@ namespace A08_Tablice
     {
         static void Main(string[] args)
         {
-            
+            //tablica stała
+            string[] tab1 = { "N", "Pn", "Wt", "Śr", "Cz", "Pt", "Sb" };
+            Console.WriteLine(tab1[3]);
+            for(int i=0; i<tab1.Length; i++) { Console.WriteLine(tab1[i]); }
+            //tablica utworzona za  pomocą konstruktora
+            int n = 10;
+            int[] tab2 = new int[n]; //konstruktor 
+            for(int i=0; i<n; i++)
+            {
+                tab2[i] = i*2;
+                Console.Write(tab2[i] + " ");
+            }
+            //Console.WriteLine();
         }
     }
 }

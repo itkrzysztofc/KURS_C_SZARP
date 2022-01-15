2aa7a2804749e5afc9dd977c616f2a13a7c8f1a4
Krzysztof C <itkrzysztofc@hotmail.com>
2022-01-15 09:06:16 +0100

    Lekcja 7.

--- A07_Skok_Break_continue/Program.cs
+++ A07_Skok_Break_continue/Program.cs
@@ -8,7 +8,40 @@ namespace A07_Skok_Break_continue
     {
         static void Main(string[] args)
         {
-           
+             //BREAK
+             int a = 1;
+            while (true)
+            {
+                if(a > 10) break;
+                Console.Write(a + " ");
+                a++;
+            }
+            Console.WriteLine("");
+            //CONTINUE
+            for(int i = 0; i < 10; i++)
+            {
+                if (i % 3 == 1)
+                {
+                    Console.WriteLine();
+                    continue;
+                    Console.WriteLine("Tu się nic nie wypisze");
+                }
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        //skok GOTO
+        etykieta1:
+            Console.WriteLine("Jesteś pod etykietą nr.1");
+            goto etykieta3;
+        etykieta2:
+            Console.WriteLine("Jesteś pod etykietą nr.2");
+            goto koniec;
+        etykieta3:
+            Console.WriteLine("Jesteś pod etykietą nr.3");
+            goto etykieta2;
+        koniec:
+            Console.WriteLine("Jesteś pod etykietą 'koniec'");
+            return;
         }  
     }
 }

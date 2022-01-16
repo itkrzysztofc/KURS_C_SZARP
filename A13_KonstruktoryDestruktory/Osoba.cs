6108ad6f5db24f7944048f5f02373abb167ffc2c
Krzysztof C <itkrzysztofc@hotmail.com>
2022-01-16 09:31:21 +0100

    Lekcja 13.

---
+++ A13_KonstruktoryDestruktory/Osoba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A13_KonstruktoryDestruktory
+{
+    internal class Osoba
+    {   //konstruktor musi być na początku klasy
+        public Osoba() //musi mieć nazwę klasy w któej się znajduje
+        {
+        }
+        public Osoba(string noweImie="", string noweNazwisko="")
+        {
+            imie = noweImie;
+            nazwisko = noweNazwisko;
+        }
+        public string imie = "BRAK IMIENIA";
+        public string nazwisko = "BRAK NAZWISKA";
+        public void Czlowiek()
+        {
+            Console.WriteLine("I'm " + imie + " " + nazwisko);
+        }
+        //destruktor jest zawsze na koniec klasy
+        ~Osoba() { }// DESTRUKTOR usuwa obiekt z pamięci po użyciw w programie
+    }
+}
---
+++ A13_KonstruktoryDestruktory/Program.cs
@@ -0,0 +1,20 @@
+﻿// A13 Konstruktory i destruktory
+using System;
+
+namespace A13_KonstruktoryDestruktory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba = new Osoba();//tworzymy obiekt - KONSTRUKTOR
+            Console.WriteLine(osoba.imie + " " + osoba.nazwisko);
+            osoba.Czlowiek(); //zastosowanie funkcji w klasie upraszcza kod Main()
+            // wysłanie parametrów do klasy
+            Osoba osoba2 = new Osoba("Jan");
+            Osoba osoba3 = new Osoba("Jan","Niezbędny");
+            Console.WriteLine(osoba2.imie + " " + osoba2.nazwisko);
+            Console.WriteLine(osoba3.imie + " " + osoba3.nazwisko);
+        }
+    }
+}

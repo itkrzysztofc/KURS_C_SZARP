2980790baee91c016cd1a52820eab8fd148bacd6
Krzysztof C <itkrzysztofc@hotmail.com>
2022-03-05 11:57:20 +0100

    Lekcja 22.

---
+++ A22_Struktury/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace A22_Struktury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Użycie struktury
+            //Odcinek dlugosciXY;
+            //dlugosciXY.x = 1;
+            //dlugosciXY.y = 1;
+
+            //2. użycie konstruktora
+            Odcinek odcinek = new Odcinek(2,5);
+            Odcinek odcinek2;
+            odcinek2 = odcinek;
+            odcinek2.x = 8;
+            Console.WriteLine("X = " + odcinek.x);
+            Console.WriteLine("Y = " + odcinek.y);
+            Console.WriteLine("a: " + odcinek.odcinek()) ;
+            Console.WriteLine("X = " + odcinek2.x);
+            Console.WriteLine("Y = " + odcinek2.y);
+            Console.WriteLine("a: " + odcinek2.odcinek());
+        }
+    }
+}
---
+++ A22_Struktury/Punkt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace A22_Struktury
+{
+    public struct Odcinek
+    {
+        public Odcinek(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int x;
+        public int y;
+        public double odcinek()
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(x,2) + Math.Pow(y,2)), 2);
+        }
+    }
+}

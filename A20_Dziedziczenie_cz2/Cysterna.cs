1a6d97bf07898ecf2d6ff1b84e090aef38e89ede
Krzysztof C <itkrzysztofc@hotmail.com>
2022-03-05 10:54:46 +0100

    Lekcja 20.

--- A20_Dziedziczenie_cz2/Cysterna.cs
+++ A20_Dziedziczenie_cz2/Cysterna.cs
@@ -6,7 +6,7 @@ namespace A20_Dziedziczenie_cz2
     {
         public Cysterna(string paliwo, int ile_paliwa, int pojemnosc) : base(paliwo, ile_paliwa)
         {
-            this.pojemnosc = pojemnosc;
+            this.pojemnosc = pojemnosc; 
         }
         public override void Ruch() //przesłonięcie motody funkc. w klasie Lory
         {

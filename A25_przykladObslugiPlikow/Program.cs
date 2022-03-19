b6c9cbf3826f4c08664734dd90faac2c36c8de62
Krzysztof C <itkrzysztofc@hotmail.com>
2022-03-19 12:19:01 +0100

    Lekcja 25. - Przykład do l.24

--- A25_przykladObslugiPlikow/Program.cs
+++ A25_przykladObslugiPlikow/Program.cs
@@ -31,21 +31,21 @@ namespace A25_przykladObslugiPlikow
             while ((linia = sr.ReadLine()) != null)
             {
                 //1. ile długich linii
-                //if (linia.Length >= 38)
-                //{
-                //    counter++;
-                //    Console.WriteLine(linia);
-                //}
+                if (linia.Length >= 38)
+                {
+                    counter++;
+                    Console.WriteLine(linia);
+                }
                 //2. Najdłuższa linia
-                //if (linia.Length > x) { x = linia.Length; }
+                if (linia.Length > x) { x = linia.Length; }
                 //3. ile jest literak " " ?
-                //for (int i = 0; i < linia.Length; i++)
-                //{
-                //    if((linia[i]) == ' ')
-                //    {
-                //        counter2++;
-                //    }
-                //}
+                for (int i = 0; i < linia.Length; i++)
+                {
+                    if((linia[i]) == ' ')
+                    {
+                        counter2++;
+                    }
+                }
                 //4. Wypisz liczby z początku pomijając resztę linii
                 for (int i = 0; i < linia.Length; i++)
                 {
@@ -65,10 +65,10 @@ namespace A25_przykladObslugiPlikow
                 nowaLinia = "";
             }
             sr.Close();
-            //Console.WriteLine("\n" + counter + "\n");// 1. ilość długich linii
-            //Console.WriteLine(x + "\n");//2.
-            //Console.WriteLine(counter2 + "\n");//3.
-            Console.Write(nowaLinia + "\n");//4.
+            Console.WriteLine("\n" + counter + "\n");// 1. ilość długich linii
+            Console.WriteLine(x + "\n");//2.
+            Console.WriteLine(counter2 + "\n");//3.
+            //Console.Write(nowaLinia + "\n");//4.
         }
     }
 }

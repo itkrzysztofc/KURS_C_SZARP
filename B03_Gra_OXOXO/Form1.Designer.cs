e9fb3f8fadfdc04ba1b59d1e82ffd42d2d852039
Krzysztof C <itkrzysztofc@hotmail.com>
2022-04-23 12:47:11 +0200

    KiK_Start

---
+++ B03_Gra_OXOXO/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace B03_Gra_OXOXO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.Gracz = new System.Windows.Forms.Label();
+            this.lblGracz = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.wynikO = new System.Windows.Forms.Label();
+            this.graczO = new System.Windows.Forms.Label();
+            this.wynikX = new System.Windows.Forms.Label();
+            this.graczX = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn1.Location = new System.Drawing.Point(12, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 100);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn2.Location = new System.Drawing.Point(118, 12);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 100);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn3.Location = new System.Drawing.Point(224, 12);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(100, 100);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn4.Location = new System.Drawing.Point(12, 118);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(100, 100);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn5.Location = new System.Drawing.Point(118, 118);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(100, 100);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn6.Location = new System.Drawing.Point(224, 118);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(100, 100);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn7.Location = new System.Drawing.Point(12, 224);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(100, 100);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn8.Location = new System.Drawing.Point(118, 224);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(100, 100);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn9.Location = new System.Drawing.Point(224, 224);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(100, 100);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // Gracz
+            // 
+            this.Gracz.AutoSize = true;
+            this.Gracz.Font = new System.Drawing.Font("Arial", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Gracz.Location = new System.Drawing.Point(368, 76);
+            this.Gracz.Name = "Gracz";
+            this.Gracz.Size = new System.Drawing.Size(217, 36);
+            this.Gracz.TabIndex = 9;
+            this.Gracz.Text = "Ruch gracza:";
+            // 
+            // lblGracz
+            // 
+            this.lblGracz.AutoSize = true;
+            this.lblGracz.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGracz.Location = new System.Drawing.Point(591, 76);
+            this.lblGracz.Name = "lblGracz";
+            this.lblGracz.Size = new System.Drawing.Size(40, 36);
+            this.lblGracz.TabIndex = 10;
+            this.lblGracz.Text = "O";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(368, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 36);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Wynik:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // wynikO
+            // 
+            this.wynikO.AutoSize = true;
+            this.wynikO.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wynikO.Location = new System.Drawing.Point(535, 224);
+            this.wynikO.Name = "wynikO";
+            this.wynikO.Size = new System.Drawing.Size(33, 36);
+            this.wynikO.TabIndex = 14;
+            this.wynikO.Text = "0";
+            // 
+            // graczO
+            // 
+            this.graczO.AutoSize = true;
+            this.graczO.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.graczO.Location = new System.Drawing.Point(368, 224);
+            this.graczO.Name = "graczO";
+            this.graczO.Size = new System.Drawing.Size(142, 36);
+            this.graczO.TabIndex = 13;
+            this.graczO.Text = "Gracz O:";
+            // 
+            // wynikX
+            // 
+            this.wynikX.AutoSize = true;
+            this.wynikX.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wynikX.Location = new System.Drawing.Point(535, 269);
+            this.wynikX.Name = "wynikX";
+            this.wynikX.Size = new System.Drawing.Size(33, 36);
+            this.wynikX.TabIndex = 16;
+            this.wynikX.Text = "0";
+            // 
+            // graczX
+            // 
+            this.graczX.AutoSize = true;
+            this.graczX.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.graczX.Location = new System.Drawing.Point(368, 269);
+            this.graczX.Name = "graczX";
+            this.graczX.Size = new System.Drawing.Size(138, 36);
+            this.graczX.TabIndex = 15;
+            this.graczX.Text = "Gracz X:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 338);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "(Krzysztof C.)";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Arial", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnRestart.Location = new System.Drawing.Point(374, 338);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(316, 72);
+            this.btnRestart.TabIndex = 18;
+            this.btnRestart.Text = "Nowa Gra";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wynikX);
+            this.Controls.Add(this.graczX);
+            this.Controls.Add(this.wynikO);
+            this.Controls.Add(this.graczO);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblGracz);
+            this.Controls.Add(this.Gracz);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kółko i Krzyżyk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label Gracz;
+        private System.Windows.Forms.Label lblGracz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label wynikO;
+        private System.Windows.Forms.Label graczO;
+        private System.Windows.Forms.Label wynikX;
+        private System.Windows.Forms.Label graczX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ B03_Gra_OXOXO/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B03_Gra_OXOXO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //zmienne początkowe ------
+        bool gracz1 = true;//zaczyna grę gracz 1
+        int ruch = 0; //ile ruchów wykonano
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if(ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+
+        private void Sprawdz()
+        {
+            //////dokończyć
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            Restartuj();
+            wynikO.Text = "0";
+            wynikX.Text = "0";
+            lblGracz.Text = "0";
+            gracz1 = true;
+        }
+
+        private void Restartuj()
+        {
+            //sposób 1 
+            //btn1.Text = ""; ...
+            ruch = 0;
+            //sposób 2 tablica przycisków
+            Button[] all = new Button[9]; //knstruktor tablicy przycisków
+            all[0] = btn1; all[1] = btn2; all[2] = btn3;
+            all[3] = btn4; all[4] = btn5; all[5] = btn6;
+            all[6] = btn7; all[7] = btn8; all[8] = btn9;
+            foreach(Button b in all)
+            {
+                b.Enabled = true; //włączamy dostępność przycisków prze resecie
+                b.Text = "";
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            ruch++;
+            //funkcja IF
+            ((Button)sender).Text = gracz1 ? "0" : "X";
+            ((Button)sender).Enabled = false;
+            if (ruch > 4)
+            {
+                Sprawdz();
+            }
+            gracz1 = !gracz1;
+            lblGracz.Text = gracz1 ? "O" : "X";
+        }
+    }
+}
